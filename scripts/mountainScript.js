6cf9cc60044c0d78d2204adc5791946a0b28e095
Yoseph Tefera <jtefera34@gmail.com>
2024-05-22 14:59:07 -0400

    functions working, fixed few font styles

--- scripts/mountainScript.js
+++ scripts/mountainScript.js
@@ -1,87 +1,93 @@
-window.onload = () => {
-    const mountainSelect = document.getElementById('mountainSelect');
+window.onload = function() {
+    const mountainSelect = document.querySelector('#mountainSelect');
 
     for (let i = 0; i < mountainsArray.length; i++) {
         const option = document.createElement('option');
         option.value = mountainsArray[i].name;
         option.textContent = mountainsArray[i].name;
         mountainSelect.appendChild(option);
+    }
+};
+ 
+    window.displayMountainInfo = function() {
+        let selectedMountain = null;
 
         
-    }
-
-    // Function to display mountain information in a dynamically created modal
-    window.displayMountainInfo = function() {
-        const selectedMountain = mountainsArray.find(mountain => mountain.name === mountainSelect.value);
-        if (selectedMountain) {
-            // Clear any existing modal content
+        for (let i = 0; i < mountainsArray.length; i++) {
+            if (mountainsArray[i].name === mountainSelect.value) {
+                selectedMountain = mountainsArray[i];
+                break; 
+            }
+        }
+        
+               if (selectedMountain) {
+            
             const existingModal = document.getElementById('dynamicModal');
             if (existingModal) {
                 existingModal.remove();
             }
 
-            // Create modal elements
+        
             const modal = document.createElement('div');
-            modal.className = 'modal fade';
+            modal.className = 'modal modal-dialog modal-sm fade';
             modal.id = 'dynamicModal';
             modal.tabIndex = '-1';
+            // modal.setAttribute('tabIndex','-1')
             modal.setAttribute('aria-labelledby', 'modalLabel');
             modal.setAttribute('aria-hidden', 'true');
-
+            
+            document.body.appendChild(modal);
+            
             const modalDialog = document.createElement('div');
             modalDialog.className = 'modal-dialog modal-dialog-centered modal-lg';
-
+            modal.appendChild(modalDialog);
+            
             const modalContent = document.createElement('div');
             modalContent.className = 'modal-content bg-dark text-white';
-
+            modalDialog.appendChild(modalContent);
+            
             const modalHeader = document.createElement('div');
             modalHeader.className = 'modal-header';
-
+            modalContent.appendChild(modalHeader);
+            
             const modalTitle = document.createElement('h5');
             modalTitle.className = 'modal-title';
             modalTitle.id = 'modalLabel';
             modalTitle.textContent = selectedMountain.name;
-
+            modalHeader.appendChild(modalTitle);
+            
             const modalCloseButton = document.createElement('button');
             modalCloseButton.className = 'close';
             modalCloseButton.type = 'button';
             modalCloseButton.setAttribute('data-dismiss', 'modal');
             modalCloseButton.setAttribute('aria-label', 'Close');
-
+            modalHeader.appendChild(modalCloseButton);
+            
             const closeSpan = document.createElement('span');
             closeSpan.setAttribute('aria-hidden', 'true');
             closeSpan.innerHTML = '&times;';
-
+            modalCloseButton.appendChild(closeSpan);
+            
             const modalBody = document.createElement('div');
             modalBody.className = 'modal-body p-0';
-
+            modalContent.appendChild(modalBody);
+            
             const mountainImage = document.createElement('img');
             mountainImage.src = `images/${selectedMountain.img}`;
             mountainImage.alt = selectedMountain.name;
             mountainImage.className = 'img-fluid w-100';
-
+            modalBody.appendChild(mountainImage);
+            
             const modalTextContainer = document.createElement('div');
             modalTextContainer.className = 'p-3 bg-dark-transparent';
-
+            modalBody.appendChild(modalTextContainer);
+            
             const mountainDescription = document.createElement('p');
             mountainDescription.textContent = selectedMountain.desc;
-
-            // Append elements to the modal
-            modalCloseButton.appendChild(closeSpan);
-            modalHeader.appendChild(modalTitle);
-            modalHeader.appendChild(modalCloseButton);
             modalTextContainer.appendChild(mountainDescription);
-            modalBody.appendChild(mountainImage);
-            modalBody.appendChild(modalTextContainer);
-            modalContent.appendChild(modalHeader);
-            modalContent.appendChild(modalBody);
-            modalDialog.appendChild(modalContent);
-            modal.appendChild(modalDialog);
-            document.body.appendChild(modal);
-
-            // Show the modal using Bootstrap's modal method
+            
+   
             const bootstrapModal = new bootstrap.Modal(modal);
             bootstrapModal.show();
         }
     }
-};
--- scripts/nationalParkSearch.js
+++ scripts/nationalParkSearch.js
@@ -1,125 +1,153 @@
 window.onload = function() {
-  const locationSelect = document.getElementById('locationSelect');
-  const parkTypeSelect = document.getElementById('parkTypeSelect');
-  const resultsList = document.getElementById('resultsList');
-  const viewAllBtn = document.getElementById('viewAllBtn');
-  const searchBtn = document.getElementById('searchBtn');
-  const prevBtn = document.getElementById('prevBtn');
-  const nextBtn = document.getElementById('nextBtn');
-  const filterOptionsBtn = document.getElementById('filterOptionsBtn');
-  const locationContainer = document.getElementById('locationContainer');
-  const parkTypeContainer = document.getElementById('parkTypeContainer');
+    const locationSelect = document.getElementById('locationSelect');
+    const parkTypeSelect = document.getElementById('parkTypeSelect');
+    const resultsList = document.getElementById('resultsList');
+    const viewAllBtn = document.getElementById('viewAllBtn');
+    const searchBtn = document.getElementById('searchBtn');
+    const filterOptionsBtn = document.getElementById('filterOptionsBtn');
+    const locationContainer = document.getElementById('locationContainer');
+    const parkTypeContainer = document.getElementById('parkTypeContainer');
 
-  let currentPage = 1;
-  const itemsPerPage = 10;
-  let currentResults = [];
+    // Function to populate location checkboxes
+    function populateLocationSelect() {
+        for (let i = 0; i < locationsArray.length; i++) {
+            const location = locationsArray[i];
+            const div = document.createElement('div');
+            div.classList.add('form-check');
+            
+            const input = document.createElement('input');
+            input.classList.add('form-check-input');
+            input.type = 'checkbox';
+            input.value = location;
+            input.id = 'location' + i;
+            
+            const label = document.createElement('label');
+            label.classList.add('form-check-label');
+            label.htmlFor = 'location' + i;
+            label.textContent = location;
+            
+            div.appendChild(input);
+            div.appendChild(label);
+            locationSelect.appendChild(div);
+        }
+    }
 
-  // Populate location checkboxes
-  for (let i = 0; i < locationsArray.length; i++) {
-      const div = document.createElement('div');
-      div.classList.add('form-check');
-      div.innerHTML = `
-          <input class="form-check-input" type="checkbox" value="${locationsArray[i]}" id="location${i}">
-          <label class="form-check-label" for="location${i}">
-              ${locationsArray[i]}
-          </label>
-      `;
-      locationSelect.appendChild(div);
-  }
+    // Function to populate park type checkboxes
+    function populateParkTypeSelect() {
+        for (let i = 0; i < parkTypesArray.length; i++) {
+            const parkType = parkTypesArray[i];
+            const div = document.createElement('div');
+            div.classList.add('form-check');
+            
+            const input = document.createElement('input');
+            input.classList.add('form-check-input');
+            input.type = 'checkbox';
+            input.value = parkType;
+            input.id = 'parkType' + i;
+            
+            const label = document.createElement('label');
+            label.classList.add('form-check-label');
+            label.htmlFor = 'parkType' + i;
+            label.textContent = parkType;
+            
+            div.appendChild(input);
+            div.appendChild(label);
+            parkTypeSelect.appendChild(div);
+        }
+    }
 
-  // Populate park type checkboxes
-  for (let i = 0; i < parkTypesArray.length; i++) {
-      const div = document.createElement('div');
-      div.classList.add('form-check');
-      div.innerHTML = `
-          <input class="form-check-input" type="checkbox" value="${parkTypesArray[i]}" id="parkType${i}">
-          <label class="form-check-label" for="parkType${i}">
-              ${parkTypesArray[i]}
-          </label>
-      `;
-      parkTypeSelect.appendChild(div);
-  }
+    // Populate the select elements with data
+    populateLocationSelect();
+    populateParkTypeSelect();
 
-  // Function to display results with pagination
-  function displayResults(parks) {
-      resultsList.innerHTML = '';
-      const totalPages = Math.ceil(parks.length / itemsPerPage);
-      const startIndex = (currentPage - 1) * itemsPerPage;
-      const endIndex = Math.min(startIndex + itemsPerPage, parks.length);
-      const pageResults = parks.slice(startIndex, endIndex);
+    // Function to display results
+    function displayResults(parks) {
+        resultsList.innerHTML = '';
+        if (parks.length === 0) {
+            const li = document.createElement('li');
+            li.classList.add('list-group-item');
+            li.textContent = 'No results found.';
+            resultsList.appendChild(li);
+        } else {
+            for (let i = 0; i < parks.length; i++) {
+                const park = parks[i];
+                const li = document.createElement('li');
+                li.classList.add('list-group-item');
 
-      if (pageResults.length === 0) {
-          const li = document.createElement('li');
-          li.classList.add('list-group-item');
-          li.textContent = 'No results found.';
-          resultsList.appendChild(li);
-      } else {
-          for (let i = 0; i < pageResults.length; i++) {
-              const park = pageResults[i];
-              const li = document.createElement('li');
-              li.classList.add('list-group-item');
-              li.innerHTML = `
-                  <h5>${park.LocationName}</h5>
-                  <p>${park.Address ? park.Address + ', ' : ''}${park.City ? park.City + ', ' : ''}${park.State ? park.State + ' ' : ''}${park.ZipCode ? park.ZipCode : ''}</p>
-                  ${park.Phone ? `<p>Phone: ${park.Phone}</p>` : ''}
-                  ${park.Fax ? `<p>Fax: ${park.Fax}</p>` : ''}
-                  ${park.Visit ? `<p><a href="${park.Visit}" target="_blank">Visit Website</a></p>` : ''}
-              `;
-              resultsList.appendChild(li);
-          }
-      }
+                const h5 = document.createElement('h5');
+                h5.classList.add('location-name');
+                h5.textContent = park.LocationName;
+                li.appendChild(h5);
 
-      prevBtn.classList.toggle('hidden', currentPage === 1);
-      nextBtn.classList.toggle('hidden', currentPage === totalPages);
-  }
+                const address = document.createElement('p');
+                address.classList.add('address');
+                address.textContent = `${park.Address ? park.Address + ', ' : ''}${park.City ? park.City + ', ' : ''}${park.State ? park.State + ' ' : ''}${park.ZipCode ? park.ZipCode : ''}`;
+                li.appendChild(address);
 
-  // Function to get selected values from checkboxes
-  function getSelectedValues(containerId) {
-      const checkboxes = document.querySelectorAll(`#${containerId} input[type="checkbox"]:checked`);
-      return Array.from(checkboxes).map(checkbox => checkbox.value);
-  }
+                if (park.Phone) {
+                    const phone = document.createElement('p');
+                    phone.classList.add('phone');
+                    phone.textContent = `Phone: ${park.Phone}`;
+                    li.appendChild(phone);
+                }
 
-  // Filter Options button click event
-  filterOptionsBtn.onclick = function() {
-      locationContainer.classList.toggle('hidden');
-      parkTypeContainer.classList.toggle('hidden');
-  };
+                if (park.Fax) {
+                    const fax = document.createElement('p');
+                    fax.classList.add('fax');
+                    fax.textContent = `Fax: ${park.Fax}`;
+                    li.appendChild(fax);
+                }
 
-  // Search button click event
-  searchBtn.onclick = function() {
-      const selectedLocations = getSelectedValues('locationSelect');
-      const selectedParkTypes = getSelectedValues('parkTypeSelect');
+                if (park.Visit) {
+                    const visit = document.createElement('p');
+                    visit.classList.add('visit');
+                    const visitLink = document.createElement('a');
+                    visitLink.href = park.Visit;
+                    visitLink.textContent = 'Visit Website';
+                    visitLink.target = '_blank';
+                    visit.appendChild(visitLink);
+                    li.appendChild(visit);
+                }
 
-      currentResults = nationalParksArray.filter(park => {
-          const locationMatch = selectedLocations.length === 0 || selectedLocations.includes(park.State);
-          const parkTypeMatch = selectedParkTypes.length === 0 || selectedParkTypes.some(type => park.LocationName.includes(type));
-          return locationMatch && parkTypeMatch;
-      });
+                resultsList.appendChild(li);
+            }
+        }
+    }
 
-      currentPage = 1;
-      displayResults(currentResults);
-  };
+    //  to get selected values
+    function getSelectedValues(containerId) {
+        const checkboxes = document.querySelectorAll(`#${containerId} input[type="checkbox"]:checked`);
+        const selectedValues = [];
+        for (let i = 0; i < checkboxes.length; i++) {
+            selectedValues.push(checkboxes[i].value);
+        }
+        return selectedValues;
+    }
 
-  // View All button click event
-  viewAllBtn.onclick = function() {
-      currentResults = nationalParksArray;
-      currentPage = 1;
-      displayResults(currentResults);
-  };
+    // Toggle 
+    filterOptionsBtn.onclick = function() {
+        locationContainer.classList.toggle('hidden');
+        parkTypeContainer.classList.toggle('hidden');
+    };
 
-  // Pagination buttons click events
-  prevBtn.onclick = function() {
-      if (currentPage > 1) {
-          currentPage--;
-          displayResults(currentResults);
-      }
-  };
+    // Search button
+    searchBtn.onclick = function() {
+        const selectedLocations = getSelectedValues('locationSelect');
+        const selectedParkTypes = getSelectedValues('parkTypeSelect');
 
-  nextBtn.onclick = function() {
-      const totalPages = Math.ceil(currentResults.length / itemsPerPage);
-      if (currentPage < totalPages) {
-          currentPage++;
-          displayResults(currentResults);
-      }
-  };
+        const filteredParks = nationalParksArray.filter(function(park) {
+            const locationMatch = selectedLocations.length === 0 || selectedLocations.includes(park.State);
+            const parkTypeMatch = selectedParkTypes.length === 0 || selectedParkTypes.some(function(type) {
+                return park.LocationName.includes(type);
+            });
+            return locationMatch && parkTypeMatch;
+        });
+
+        displayResults(filteredParks);
+    };
+
+    // View All button 
+    viewAllBtn.onclick = function() {
+        displayResults(nationalParksArray);
+    };
 };
